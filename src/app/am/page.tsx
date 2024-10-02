20ecbb902d3f72e3193d4f92ea8e4a5738460b72
Bungle <hn.kim@ecubelabs.com>
2024-10-02 14:15:17 +0900

    feat: am 페이지

---
+++ src/app/am/page.tsx
@@ -0,0 +1,15 @@
+import Feature from "@/components/Feature";
+import { getObject } from "@/utils/s3";
+
+async function AMPage() {
+  const json = await getObject("2024-07-09-feature.json");
+
+  return (
+    <div className="h-full w-full bg-gray-100 p-4 text-blue-700 text-blue-700">
+      <h1 className="font-bold text-[28px]">AM Characteristic Analysis</h1>
+      <Feature data={json} />
+    </div>
+  );
+}
+
+export default AMPage;
---
+++ src/components/Feature.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { useState } from "react";
+
+function Feature(props: { data: any }) {
+  const { data } = props;
+  const [selected, setSelected] = useState<null | {
+    email: string;
+    feature: string;
+    characteristic: [];
+    violation: [];
+  }>(null);
+
+  return (
+    <div className="flex gap-8 h-3/5">
+      <ul className="flex flex-col gap-4 max-h-screen overflow-y-auto shrink-0">
+        {data.map((item: any) => (
+          <li
+            onClick={() => setSelected(item)}
+            key={item.email}
+            className={`bg-white text-black rounded-md p-4 ${
+              selected?.email === item.email ? "bg-blue-700 text-white" : ""
+            } ${item.violation.length ? "border-4 border-blue-700" : ""}`}
+          >
+            {item.email}
+          </li>
+        ))}
+      </ul>
+      {selected && (
+        <div className="bg-white rounded-md p-4 h-dvh overflow-y-auto">
+          <label className="text-black font-bold text-[20px]">Summary</label>
+          <p className="bg-gray-100 p-4 text-black mb-8 whitespace-break-spaces">
+            {selected.feature}
+          </p>
+          <label className="text-black font-bold text-[20px]">Feature</label>
+          <ul className="max-h-[30%] overflow-y-auto mb-8">
+            {selected.characteristic.map((item: any, index: number) => (
+              <li
+                key={index}
+                className="bg-gray-100 p-4 text-black rounded-md mb-4"
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+          <label className="text-black font-bold text-[20px]">Violation</label>
+          <ul>
+            {selected.violation.length ? (
+              selected.violation.map((item: any, index: number) => (
+                <li
+                  key={index}
+                  className="bg-gray-100 p-4 text-black rounded-md"
+                >
+                  <label className="text-gray-400">Violation name</label>
+                  <p>
+                    <strong>{item.name}</strong>
+                  </p>
+                  <label className="text-gray-400">Evidence</label>
+                  <p>{item.quote}</p>
+                  <label className="text-gray-400">Confidence</label>
+                  <div
+                    className={`text-white text-center text-[20px] ${
+                      item.confidence === "high"
+                        ? "bg-red-500 w-9/10"
+                        : item.confidence === "medium"
+                        ? "bg-yellow-500 w-6/10"
+                        : "bg-green-500 w-3/10"
+                    }`}
+                  >
+                    {item.confidence}
+                  </div>
+                </li>
+              ))
+            ) : (
+              <li className="bg-gray-100 p-4 text-black rounded-md">
+                😊 No violation found
+              </li>
+            )}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Feature;
